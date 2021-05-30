5a9f96827893cd63c363b74fae1a4b4e68827029
sujan011 <37547799+sujan011@users.noreply.github.com>
2021-05-30 18:54:13 +0545

    Update canvas.js

--- src/three-js/canvas.js
+++ src/three-js/canvas.js
@@ -87,7 +87,7 @@ const useStore = create((set) => {
             {
               position: [-2, -2, -10],
               r: 0,
-              geometry: new THREE.TextGeometry("P", config)
+              geometry: new THREE.TextGeometry("S", config)
             }
           ]
         });
